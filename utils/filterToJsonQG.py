6635397cd6131f594446665757db8c8cbee75492
albonwu <albonwu@umich.edu>
2023-05-09 21:17:38 -0500

    broadened QG criteria

--- utils/filterToJsonQG.py
+++ utils/filterToJsonQG.py
@@ -6,24 +6,31 @@ qg = open("../tsv/filteredQG.tsv", "w")
 jsonOut = open("../hierarchyQG.json", "w")
 
 variations = {}
-for line in d_openings:
+iterator = iter(d_openings)
+next(iterator)  # skip line 1, which holds column titles
+
+for line in iterator:
     firstWord = line.split(" ")[0]
-    if firstWord == "Slav" or firstWord == "Semi-Slav":
-        line = "Queen's Gambit Declined: " + line
-    if "Queen's Gambit" in line:
+    separated = line.split("	")
+    dco = int((separated[0][1:]))
+
+    if (
+        "Queen's Gambit" in line
+        or (dco >= 30 and dco <= 69)
+        or (dco >= 10 and dco <= 20)
+    ):
         # invariant: ECO + U+0009 + name + U+0009 + AN
-        separated = line.split("	")
         variations[separated[1]] = separated[2]
-    
+
 for entry in variations:
     qg.write(entry + " " + variations[entry])
 
 hierarchy = {}
 # transfer to dictionary
 for name in variations.keys():
-    path = name.replace(': ', ', ')
-    path = path.replace(' Declined', ', Declined')
-    path = path.replace(' Accepted', ', Accepted').split(', ')
+    path = name.replace(": ", ", ")
+    path = path.replace(" Declined", ", Declined")
+    path = path.replace(" Accepted", ", Accepted").split(", ")
 
     current = hierarchy
     for sub in path:
@@ -31,10 +38,11 @@ for name in variations.keys():
             current[sub] = {}
         current = current[sub]
 
+
 # format json according to d3 tidy tree
 def reformat(hierarchy):
     ans = []
-    
+
     for entry in hierarchy.keys():
         res = defaultdict(list)
         res["name"] = entry
@@ -42,7 +50,8 @@ def reformat(hierarchy):
         if len(children) != 0:
             res["children"] = reformat(children)
         ans.append(res)
-    
+
     return ans
 
-jsonOut.write(json.dumps(reformat(hierarchy)[0], indent=2, separators=(',', ': ')))
+
+jsonOut.write(json.dumps(reformat(hierarchy)[0], indent=2, separators=(",", ": ")))
